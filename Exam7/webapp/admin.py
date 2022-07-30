b3490c3895c0a3eea2bf9566b8c0030201da37e9
akim <akimmmm@bk.ru>
2022-07-30 12:03:25 +0600

    создал полл и чойс в админке и модели

--- Exam7/webapp/admin.py
+++ Exam7/webapp/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+from webapp.models import Poll, Choice
+
+
+class PollAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'created_at']
+    list_display_links = ['name']
+    list_filter = ['created_at']
+    search_fields = ['created_at']
+    fields = ['name']
+    readonly_fields = []
+    filter_horizontal = []
+
+
+admin.site.register(Poll, PollAdmin)
+admin.site.register(Choice)
--- Exam7/webapp/models.py
+++ Exam7/webapp/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Poll(models.Model):
+    name = models.TextField(max_length=50, null=False, blank=False, verbose_name="Название")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+
+    def __str__(self):
+        return f"{self.id}. {self.name}"
+
+    class Meta:
+        db_table = "polls"
+        verbose_name = "Вопрос"
+        verbose_name_plural = "Вопросы"
+
+
+class Choice(models.Model):
+    name = models.TextField(max_length=50, null=False, blank=False, verbose_name="Название")
+    status = models.ForeignKey("webapp.Poll", on_delete=models.CASCADE, related_name="polls",
+                               verbose_name='Вопрос')
+
+    def __str__(self):
+        return f"{self.id}. {self.name}"
+
+    class Meta:
+        db_table = "choices"
+        verbose_name = "Выбор"
+        verbose_name_plural = "Выборы"
